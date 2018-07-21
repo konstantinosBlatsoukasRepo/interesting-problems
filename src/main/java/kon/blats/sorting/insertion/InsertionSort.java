bf97ec763ea978b64fb034ab953f0e02b9265298
kostas <konstantinosblatsoukasrepo@gmail.com>
2018-07-21 10:30:02 +0300

    Add insertion sort

---
+++ src/main/java/kon/blats/sorting/insertion/InsertionSort.java
@@ -0,0 +1,32 @@
+package kon.blats.sorting.insertion;
+
+import java.util.Objects;
+
+/**
+ * Created by kon on 21/7/2018.
+ */
+public class InsertionSort {
+
+    public static int[] sort(int[] table) {
+        Objects.requireNonNull(table);
+
+        for (int i = 0; i < table.length; i++) {
+            for (int j = i; j > 0 && table[j - 1] > table[j]; j--) {
+                swap(j, j - 1, table);
+            }
+        }
+
+        return table;
+    }
+
+    public static void swap(int i, int j, int [] table) {
+        int temp = table[i];
+        table[i] = table[j];
+        table[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] testTable = new int[]{3, 2, 1, 45, 34, 5};
+        sort(testTable);
+    }
+}
