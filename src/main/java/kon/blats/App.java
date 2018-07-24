3e81dc609d650813d787bc87272782d77bf86005
kostas <konstantinosblatsoukasrepo@gmail.com>
2018-07-24 22:03:18 +0300

    Add correction in calculation

--- src/main/java/kon/blats/App.java
+++ src/main/java/kon/blats/App.java
@@ -1,43 +1,36 @@
 package kon.blats;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 
 import static java.lang.Math.abs;
 
 public class App {
     public static void main(String[] args) {
 
-        String desiredCutOffTime = "05:30";
+        String desiredCutOffTime = "05:00";
         int desiredHour = Integer.parseInt(desiredCutOffTime.split(":")[0]);
         int desiredMinutes = Integer.parseInt(desiredCutOffTime.split(":")[1]);
 
-        ZonedDateTime utcZonedDateTime = ZonedDateTime.now();
-        ZonedDateTime malZonedDateTime = utcZonedDateTime.withZoneSameInstant(ZoneId.of("+12"));
+        ZonedDateTime utcZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime bspTimeNow = utcZonedDateTime.withZoneSameInstant(ZoneId.of("+8"));
 
-        int hourNowInMalaysia = malZonedDateTime.getHour();
-        int minuteNowInMalaysia = malZonedDateTime.getMinute();
+        int hourNowInBsp = bspTimeNow.getHour();
+        int minuteNowInBsp = bspTimeNow.getMinute();
 
-        boolean isPositiveHoursDiff = desiredHour - hourNowInMalaysia > 0;
-        boolean isPositiveMinutesDiff = desiredMinutes - minuteNowInMalaysia > 0;
+        boolean isPositiveHoursDiff = desiredHour - hourNowInBsp > 0;
+        LocalTime bspTime = LocalTime.of(hourNowInBsp, minuteNowInBsp);
+        LocalTime desiredTime = LocalTime.of(desiredHour, desiredMinutes);
 
-        if (isPositiveHoursDiff && isPositiveMinutesDiff) {
-            LocalTime malaisyanTime = LocalTime.of(hourNowInMalaysia, minuteNowInMalaysia);
-            LocalTime desiredTime = LocalTime.of(desiredHour, desiredMinutes);
-            System.out.println(Duration.between(malaisyanTime, desiredTime).getSeconds());
+        long secondsToCutOff;
+        if (isPositiveHoursDiff) {
+            secondsToCutOff = Duration.between(bspTime, desiredTime).getSeconds();
         } else {
-            LocalTime malaisyanTime = LocalTime.of(hourNowInMalaysia, minuteNowInMalaysia);
-            LocalTime desiredTime = LocalTime.of(desiredHour, desiredMinutes);
-
             LocalTime midnightTime = LocalTime.of(23, 59, 59, 99);
-            long midnightDiff = abs(Duration.between(malaisyanTime, midnightTime).getSeconds());
+            long midnightDiff = abs(Duration.between(bspTime, midnightTime).getSeconds());
             long absoluteDiff = abs(Duration.between(LocalTime.of(00, 00, 00, 00), desiredTime).getSeconds());
-
-            long delay = absoluteDiff + midnightDiff;
+            secondsToCutOff = absoluteDiff + midnightDiff;
         }
+        System.out.println(secondsToCutOff);
 
-        Duration difference = Duration.between(utcZonedDateTime, malZonedDateTime);
     }
 }
