e6ac323a5e9f439f1c92a9a04b8ea5232f666b76
kostas <konstantinosblatsoukasrepo@gmail.com>
2018-08-12 14:22:34 +0300

    Add checking the characters uniqueness

---
+++ src/main/java/kon/blats/character/uniqueness/CharacterUniquenessTest.java
@@ -0,0 +1,29 @@
+package kon.blats.character.uniqueness;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by kon on 12/8/2018.
+ */
+public class CharacterUniquenessTest {
+
+    public static void main(String[] args) {
+        String uniqueString = "abcd";
+        System.out.println(isUnique(uniqueString));
+
+        String nonUniqueString = "abcda";
+        System.out.println(isUnique(nonUniqueString));
+    }
+
+    private static boolean isUnique(String input) {
+        char[] characters = input.toCharArray();
+        Set<Integer> setOfCharacters = new HashSet<>();
+        for (int i = 0; i < characters.length; i++) {
+            char character = characters[i];
+            setOfCharacters.add((int) character);
+        }
+        return characters.length == setOfCharacters.size();
+    }
+
+}
